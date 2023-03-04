5eea05e62f1e214f37f8c9d4a4a47a88170887fc
Kevin <realkevinx@gmail.com>
2023-03-04 13:22:16 -0500

    Commit

---
+++ Assignments/list Assignment/listAssignment2.py
@@ -0,0 +1,7 @@
+"""
+Setting up matches for a single elimination tournament is fairly straightforward.
+All you need to do is randomly assign the first round of games and the rest is all deterministic (there are no more choices to make)
+Create a program that allows the user to enter as many names as he or she wants and display the random pairings for the first round.
+If there are an odd number of players then add in a player called "Bye" Your program should display the results in a nice neat table.
+"""
+
