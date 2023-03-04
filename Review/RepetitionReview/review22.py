e0c82161015207c12d80abb7cdde17d469793ceb
Kevin <123706048+kevinfied@users.noreply.github.com>
2023-03-04 13:00:27 -0500

    Add files via upload

---
+++ Review/RepetitionReview/review22.py
@@ -0,0 +1,12 @@
+"""
+You won $1,000,000 in the lottery, and you want to take out enough each year to live on.
+Ignoring any tax concerns, you invest the money at 9% annual interest.
+At the beginning of each year you take out your money.
+The amount of money you take out is $50,000 in the first year,
+but increases by 12% each year.
+Write a program to figure out how many years the money will last,
+and how much money you would have in the last year.
+"""
+
+balance = 1000000
+year = 0
